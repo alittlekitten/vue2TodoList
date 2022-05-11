0e5f659f94af75974b23deabfad9b1f425f1ca3c
Ssoco <dlscjs8646@gmail.com>
2022-05-12 04:49:44 +0900

    ✨ 데이터 관리 및 이벤트 작동을 위한 최상위 App 컴포넌트 생성
    - localStorage를 적용하여 껐다 켜도 데이터가 유지될 수 있도록 설정
    - key값으로 사용중인 cnt를 localStorage에서 임의로 삭제하는 경우 todoList가 created 과정에서 초기화되도록 설정

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput @addTodo="addTodo"></TodoInput>
+    <!-- @는 v-on:과 같다 -->
+    <TodoList :propsData="todoItems" :propsCnt="cnt" @removeTodo="removeTodo"></TodoList>
+    <!-- :는 v-bind:와 같다 -->
+    <TodoFooter @clearTodo="clearTodo"></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import TodoFooter from "./components/TodoFooter";
+export default {
+  name: "App",
+  data() {
+    return {
+      todoItems: [], // 아이템들
+      cnt: 0, // 고유한 key값으로 쓸 용도
+    };
+  },
+  created() {
+    if (localStorage.getItem("cnt")) {
+      // cnt 확인 (cnt가 로컬스토리지에서 지워졌다면 아이템들도 지우기)
+      this.cnt = localStorage.getItem("cnt");
+    } else {
+      localStorage.clear();
+      localStorage.setItem("cnt", 0);
+    }
+    if (localStorage.getItem("todos")) {
+      // todos 확인
+      let items = JSON.parse(localStorage.getItem("todos")); // string에서 배열로 바꾸기
+      this.todoItems = items;
+    }
+  },
+  methods: {
+    addTodo(todo) {
+      ++this.cnt;
+      this.todoItems.push({ todo: todo, cnt: this.cnt });
+      localStorage.setItem("todos", JSON.stringify(this.todoItems)); // 배열에서 string으로 바꾸기
+      localStorage.setItem("cnt", this.cnt);
+    },
+    removeTodo(todo) {
+      let items = JSON.parse(localStorage.getItem("todos"));
+      this.todoItems = items.filter((elem) => todo.cnt !== elem.cnt);
+      localStorage.setItem("todos", JSON.stringify(this.todoItems));
+    },
+    clearTodo() {
+      this.todoItems = [];
+      this.cnt = 0;
+      localStorage.clear();
+    },
+  },
+  components: {
+    TodoHeader: TodoHeader,
+    TodoInput: TodoInput,
+    TodoList: TodoList,
+    TodoFooter: TodoFooter,
+  },
+};
+</script>
+
+<style></style>
