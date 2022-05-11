880ef5a3b3a093de4e8bc6601f215d1a34da2bbd
Ssoco <dlscjs8646@gmail.com>
2022-05-12 04:46:48 +0900

    ✨ todoList 출력을 위한 TodoList 컴포넌트 생성

---
+++ src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todo in propsData" :key="todo.cnt">
+        {{ todo.todo }}
+        <button @click="removeTodo(todo)">x</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["propsData"],
+  methods: {
+    removeTodo(todo) {
+      this.$emit("removeTodo", todo);
+    },
+  },
+};
+</script>
