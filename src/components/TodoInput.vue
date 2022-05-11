875c0263f5cca8bfd22df1ce0e3fb4eaa1cb46a4
Ssoco <dlscjs8646@gmail.com>
2022-05-12 04:45:39 +0900

    ✨ todo 입력을 위한 TodoInput 컴포넌트 생성

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem" />
+    <button type="button" @click="addTodo">추가하기</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem) {
+        let value = this.newTodoItem.trim();
+        this.$emit("addTodo", value); // 이벤트를 통해 값 넘겨주기
+        this.newTodoItem = "";
+      }
+    },
+  },
+};
+</script>
