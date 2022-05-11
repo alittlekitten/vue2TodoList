cc86571a99af2d004572262ec74cb464c496a564
Ssoco <dlscjs8646@gmail.com>
2022-05-12 04:48:16 +0900

    ✨ 전체를 한번에 관리하는 용도의 TodoFooter 컴포넌트 생성

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button @click="clearTodo">전체삭제</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    clearTodo() {
+      this.$emit("clearTodo");
+    },
+  },
+};
+</script>
