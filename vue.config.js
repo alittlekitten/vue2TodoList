531e67bc4239bbacab078e7c97e3917af321999c
Ssoco <dlscjs8646@gmail.com>
2022-05-13 03:57:13 +0900

    💄 sass(.scss) 기본 설정

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,15 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/vue2TodoList",
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/_variables.scss";
+        `,
+      },
+    },
+  },
 });
+
+/* _가 붙은 scss 파일은 변수만을 저장하는 파일로 인식된다. */
