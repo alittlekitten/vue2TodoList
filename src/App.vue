250405fab45baecd48ccc66fadcd09a0d80e016d
Ssoco <dlscjs8646@gmail.com>
2022-05-13 03:57:39 +0900

    💄 페이지의 전체를 담당하는 App.vue의 CSS 적용 및 Font 설정

--- src/App.vue
+++ src/App.vue
@@ -63,4 +63,21 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Dongle&display=swap");
+#app {
+  font-family: "Dongle", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 100px;
+  background-color: $bg-color;
+  font-size: 2rem;
+}
+html,
+body {
+  background-color: $bg-color;
+  font-size: 16px;
+}
+</style>
