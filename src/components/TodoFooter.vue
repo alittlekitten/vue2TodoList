91cbb4699879817e795183c705a08a5c8f99e64c
Ssoco <dlscjs8646@gmail.com>
2022-05-13 04:04:29 +0900

    💄 TodoFooter 컴포넌트 최초 style 적용
    부트스트랩 적용, 폰트 사이즈 설정

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <button @click="clearTodo">전체삭제</button>
+  <div id="todo-footer">
+    <b-button variant="danger" size="lg" class="clear-btn" @click="clearTodo">전체삭제</b-button>
   </div>
 </template>
 
@@ -13,3 +13,11 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+#todo-footer {
+  .clear-btn {
+    font-size: 2rem;
+  }
+}
+</style>
