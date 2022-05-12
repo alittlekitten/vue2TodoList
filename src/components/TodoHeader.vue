d999eded861f9ef8c5374d84789a4a2b50d8cdd2
Ssoco <dlscjs8646@gmail.com>
2022-05-13 04:02:36 +0900

    💄 TodoHeader 컴포넌트 최초 style 적용
    title 글자색 및 폰트 크기 및 굵기 설정

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <h1>Vue2로 만드는 간단한 TodoList</h1>
+  <div id="todo-header">
+    <h1 class="title">
+      <span class="vue2">Vue2</span>로 만드는 간단한 <span class="point">TodoList</span>
+    </h1>
   </div>
 </template>
+<style lang="scss">
+#todo-header {
+  .title {
+    font-weight: 700;
+    font-size: 4rem;
+
+    .vue2 {
+      color: $title-point1;
+    }
+
+    .point {
+      color: $title-point2;
+    }
+  }
+}
+</style>
