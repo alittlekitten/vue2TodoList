f6058ab731db8e54feeeb21a5e07e711762c272f
Ssoco <dlscjs8646@gmail.com>
2022-05-13 04:03:20 +0900

    💄 TodoList 컴포넌트 최초 style 적용
    부트스트랩 적용, 버튼 크기 변경, 마진 설정

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div id="todo-list">
     <ul>
       <li v-for="todo in propsData" :key="todo.cnt">
-        {{ todo.todo }}
-        <button @click="removeTodo(todo)">x</button>
+        <span class="todo">{{ todo.todo }}</span>
+        <b-button pill size="sm" class="test" @click="removeTodo(todo)">x</b-button>
       </li>
     </ul>
   </div>
@@ -19,3 +19,22 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+#todo-list {
+  margin: 1rem 0 2rem 0;
+
+  ul {
+    list-style: none;
+  }
+
+  .todo {
+    color: $todo-color;
+    margin-right: 1rem;
+  }
+
+  .test {
+    font-size: 2rem;
+  }
+}
+</style>
